3be0324a8f2e6868bd984616890fff9784787a1c
kowalkowska-just <kowalkowska.just@gmail.com>
2020-07-13 18:06:04 +0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Justyna Kowalkowska on 13/07/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
