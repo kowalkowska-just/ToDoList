233af35d5761b160bb76360448f21443527a67c9
kowalkowska-just <kowalkowska.just@gmail.com>
2020-07-13 23:25:22 +0200

    I used Table View Controller instead of the usual Table View.

--- ToDoList/Controllers/CategoryViewController.swift
+++ ToDoList/Controllers/CategoryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController {
+class CategoryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- ToDoList/Controllers/ItemsViewController.swift
+++ ToDoList/Controllers/ItemsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ItemsViewController: UIViewController {
+class ItemsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
