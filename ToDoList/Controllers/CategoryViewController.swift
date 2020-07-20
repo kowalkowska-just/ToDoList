bee9389c4854d48aca6583c8b930c7d714a77fc4
kowalkowska-just <kowalkowska.just@gmail.com>
2020-07-20 21:51:27 +0200

    Added an alert that may add a new item to itemArray.

--- ToDoList/Controllers/CategoryViewController.swift
+++ ToDoList/Controllers/CategoryViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class CategoryViewController: UITableViewController {
 
+    let categoryArray = ["Learn", "Home", "Work", "Eat"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - TableViewDataSour
+    
+    //Number of cells
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    //Contents of the cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categoryArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Selected cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
 }
 
--- ToDoList/Controllers/ItemsViewController.swift
+++ ToDoList/Controllers/ItemsViewController.swift
@@ -9,10 +9,77 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
-    
+
+    var itemsArray = ["GIT", "Swift", "Objectiv-C", "Jira"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+//MARK: - TableView DataSource Methods
+    
+    //Number of cells
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    //Contents of the cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        
+        return cell
+    }
+
+//MARK: - TableView Delegate Methods
+
+    //Selected cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    
+//MARK: - Add New Items
+    
+    @IBAction func AddItemsButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var alertTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user click the Add Item button on our UIAlert.
+            
+            if alertTextField.text! != "" {
+                self.itemsArray.append(alertTextField.text!)
+            }
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.keyboardAppearance = .light
+            textField.keyboardType = .default
+            textField.placeholder = "Enter your new item"
+            textField.backgroundColor = UIColor.init(named: "LightGreenColor")
+            textField.textColor = UIColor.init(named: "DarkGreenColor")
+            textField.font = UIFont(name: "AmericanTypewriter", size: 19)
+            
+            alertTextField = textField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
     
 }
